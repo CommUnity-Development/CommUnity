13a1572699e0cb16e941394f77edbd047490feda
Darshan Krishnaswamy <darsh797@gmail.com>
2020-03-28 17:43:46 -0400

    Set up firebase and created Entry and EntryAdapter classes

---
+++ app/src/main/java/com/development/community/EntryAdapter.java
@@ -0,0 +1,27 @@
+package com.development.community;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class EntryAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0; //TODO: Not Implemented
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null; //TODO: Not Implemented
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0; //TODO: Not Implemented
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null; //TODO: Not Implemented
+    }
+}
