06d1aaf6225886772add5b11fec5ddc3c5c52d31
Darshan Krishnaswamy <darsh797@gmail.com>
2020-03-29 13:22:10 -0400

    Created preliminary layout for EntryActivity and implemented date and time choosers

---
+++ app/src/main/java/com/development/community/Controller.java
@@ -0,0 +1,8 @@
+package com.development.community;
+
+public class Controller {
+    public static String[] monthsShort = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+            "Oct", "Nov", "Dec"};
+    public static String[] months = {"January", "February", "March", "April", "May", "June", "July", "August",
+            "September", "October","November", "December"};
+}
