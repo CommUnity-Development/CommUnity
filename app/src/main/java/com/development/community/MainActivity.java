d83ee0b3e073dbf91843b1077f097ec31868ef89
Darshan Krishnaswamy <darsh797@gmail.com>
2020-04-01 15:45:08 -0400

    Implemented loading data from firebase into RecyclerView

--- app/src/main/java/com/development/community/MainActivity.java
+++ app/src/main/java/com/development/community/MainActivity.java
@@ -2,9 +2,11 @@ package com.development.community;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,9 +19,21 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.Map;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity implements EntryAdapter.onEntryListener{
     EntryAdapter entryAdapter;
     ArrayList<Entry> testArrayList = new ArrayList<>();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ArrayList<String> tasks = new ArrayList<>();
+    ArrayList<Time> times = new ArrayList<>();
+    ArrayList<Date> dates = new ArrayList<>();
+    ArrayList<String> locations = new ArrayList<>();
 
 
 
@@ -28,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements EntryAdapter.onEn
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("test");
 
         ImageButton postButton = findViewById(R.id.postButton);
 
@@ -39,8 +55,32 @@ public class MainActivity extends AppCompatActivity implements EntryAdapter.onEn
         });
 
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    Entry entry = ds.getValue(Entry.class);
+                    testArrayList.add(entry);
+                    assert entry != null;
+                    tasks.add(entry.getTask());
+                    times.add(entry.getTime());
+                    dates.add(entry.getDate());
+                    locations.add(entry.getDestination());
+                }
+                entryAdapter = new EntryAdapter(MainActivity.this, times, dates, locations, tasks,
+                        MainActivity.this);
+                recyclerView.setAdapter(entryAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("TAG", "onCancelled: ");
+            }
+        });
 
         testArrayList.add(new Entry(new Date(5,12,2020), new Time(14,15),
                 "Earth", "Buy groceries"));
@@ -73,17 +113,14 @@ public class MainActivity extends AppCompatActivity implements EntryAdapter.onEn
         testArrayList.add(new Entry(new Date(5,12,1987), new Time(12,18),
                 "Antarctica", "Hang out with penguins"));
 
-        ArrayList<String> tasks = new ArrayList<>();
-        ArrayList<Time> times = new ArrayList<>();
-        ArrayList<Date> dates = new ArrayList<>();
-        ArrayList<String> locations = new ArrayList<>();
+
         for(Entry item : testArrayList){
             tasks.add(item.getTask());
             times.add(item.getTime());
             dates.add(item.getDate());
             locations.add(item.getDestination());
         }
-        entryAdapter = new EntryAdapter(MainActivity.this, times,dates,locations,tasks, this);
+        entryAdapter = new EntryAdapter(MainActivity.this, times, dates, locations, tasks, this);
         recyclerView.setAdapter(entryAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
