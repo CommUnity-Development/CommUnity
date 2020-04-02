030560bd650ea368f26afc269939b394d68fdb56
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-02 14:23:04 -0400

    I updated the profile page as well as added a create account page, now we just have link the google account with the account information

---
+++ app/src/main/java/com/development/community/account_create.java
@@ -0,0 +1,14 @@
+package com.development.community;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class account_create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_create);
+    }
+}
