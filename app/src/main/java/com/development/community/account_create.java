6a0c1f642849f4507dd3377f24f15a943e406ea5
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-06 14:16:02 -0400

    Updated the account view and create activities, now we just need to connect the proper variables to firebase to store appropriate information for each user

--- app/src/main/java/com/development/community/account_create.java
+++ app/src/main/java/com/development/community/account_create.java
@@ -1,6 +1,10 @@
 package com.development.community;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,16 @@ public class account_create extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_create);
+
+
+        @SuppressLint("WrongViewCast") ImageButton createButton = findViewById(R.id.createButton);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(account_create.this, EntryActivity.class));
+            }
+        });
+
     }
 }
