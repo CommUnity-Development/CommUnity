efcac6d6c6a85c0b90fa6d49a56adb2de16712be
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-09 13:44:47 -0400

    I updated the User class by adding private data and also added in a default constructor where everything is null

--- app/src/main/java/com/development/community/User.java
+++ app/src/main/java/com/development/community/User.java
@@ -3,5 +3,18 @@ package com.development.community;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    
+    private String name;
+    private String state;
+    private String town;
+    private String address;
+    private String bio;
+
+    public User(){
+        name = "";
+        state = "";
+        town = "";
+        address = "";
+        bio = "";
+    }
+
 }
