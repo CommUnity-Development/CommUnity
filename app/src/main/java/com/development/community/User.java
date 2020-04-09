50b47063c1c6e2afd7304c947b762fadc2721347
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-09 13:47:46 -0400

    Created a constructor with data and setup the setters/getters, one more commit and the user class should be finished

--- app/src/main/java/com/development/community/User.java
+++ app/src/main/java/com/development/community/User.java
@@ -17,4 +17,52 @@ public class User implements Serializable {
         bio = "";
     }
 
+    public User(String givenName, String givenState, String givenTown, String givenAddress, String givenBio){
+        name = givenName;
+        state = givenState;
+        town = givenTown;
+        address = givenAddress;
+        bio = givenBio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
 }
