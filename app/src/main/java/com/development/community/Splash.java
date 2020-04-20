105f94a3956a4febb7d770b5d401097b08b21aac
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-20 12:54:52 -0400

    bugfix

--- app/src/main/java/com/development/community/Splash.java
+++ app/src/main/java/com/development/community/Splash.java
@@ -4,20 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Splash extends AppCompatActivity {
 
     private static final int SPLASH_TIME = 5000;
 
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("Users");
+
+    private String userState,userBio,userAddress,userTown;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent();
+
+
                 intent.setClass(Splash.this, MainActivity.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
@@ -25,4 +41,7 @@ public class Splash extends AppCompatActivity {
             }
         }, SPLASH_TIME);
     }
+
+
 }
+
