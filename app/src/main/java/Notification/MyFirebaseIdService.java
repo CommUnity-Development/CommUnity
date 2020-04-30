d7b3270156c56526d54452605eacb86372d205dd
daqureshi <daanish.a.qureshi@gmail.com>
2020-04-30 14:00:05 -0400

    major updates notifications

---
+++ app/src/main/java/Notification/MyFirebaseIdService.java
@@ -0,0 +1,31 @@
+package Notification;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseIdService extends FirebaseMessagingService {
+
+    
+    public void onTokenRefresh(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        if(firebaseUser != null){
+            updateToken(refreshedToken);
+        }
+    }
+
+    private void updateToken(String refreshedToken){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
+        Token token = new Token(refreshedToken);
+        reference.child(firebaseUser.getUid()).setValue(token);
+
+    }
+
+}
