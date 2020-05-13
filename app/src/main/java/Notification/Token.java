5ffb6cc0d2044696e765e35129a624b89f62d092
daqureshi <daanish.a.qureshi@gmail.com>
2020-05-13 15:18:41 -0400

    All classes officially are documented

--- app/src/main/java/Notification/Token.java
+++ app/src/main/java/Notification/Token.java
@@ -1,13 +1,24 @@
 package Notification;
 
+/**
+ * a class that helps deal with Tokens, stores them when necessary so they are easy to access
+ */
 public class Token {
     private String token;
 
+    /**
+     * Constructor with appropriate parameters
+     * @param token the token to be stored
+     */
     public Token(String token) {
         this.token = token;
     }
 
+    /**
+     * default constructor
+     */
     public Token() {
+        token = "";
     }
 
 
